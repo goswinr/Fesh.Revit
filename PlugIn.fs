dfecdd356608ac61ca15e5a0bb28dec3d35b3c3f
Goswin <gwinsky@yahoo.com>
2020-05-19 10:24:59 +0200

    init

--- PlugIn.fs
+++ PlugIn.fs
@@ -12,54 +12,40 @@ open Autodesk.Revit.UI
 open Autodesk.Revit.UI.Events
 open Autodesk.Revit.UI
 
+[<Transaction( TransactionMode.Manual )>]
+type OpenEditorCommand()= // don't rename !
+    interface IExternalCommand with
+        member x.Execute(commandData, message, elements) =
+
+            //https://thebuildingcoder.typepad.com/blog/2018/11/revit-window-handle-and-parenting-an-add-in-form.html
+            let winHandle = Diagnostics.Process.GetCurrentProcess().MainWindowHandle
+            
+            let seff = Seff.App.runEditorHosted(winHandle,"Revit")
+            seff.Window.Show()
+
+            Result.Succeeded
 
 
-type SeffAddin() =
+
+type SeffAddin() = // don't rename !
     interface IExternalApplication with
         member this.OnStartup(app:UIControlledApplication) =
             
-            // Create a custom ribbon tab
-            String tabName = "This Tab Name";
-            application.CreateRibbonTab(tabName);
-
-            // Create two push buttons
-            PushButtonData button1 = new PushButtonData("Button1", "My Button #1",
-                @"C:\ExternalCommands.dll", "Revit.Test.Command1");
-            PushButtonData button2 = new PushButtonData("Button2", "My Button #2",
-                @"C:\ExternalCommands.dll", "Revit.Test.Command2");
-
-            // Create a ribbon panel
-            RibbonPanel m_projectPanel = application.CreateRibbonPanel(tabName, "This Panel Name"); 
-
-            // Add the buttons to the panel
-            List projectButtons = new List();
-            projectButtons.AddRange(m_projectPanel.AddStackedItems(button1, button2));
-
-            return Result.Succeeded;
-
-
             // Method to add Tab and Panel 
-            let panel = app.CreateRibbonPanel("Seff")
-            app.CreateRibbonTab("seff Tab")
+            
+            app.CreateRibbonTab("Seff Tab")
+
             let thisAssemblyPath = Reflection.Assembly.GetExecutingAssembly().Location
 
-            // BUTTON FOR THE SINGLE-THREADED WPF OPTION
-           
-            let button = new PushButtonData("Seff", "Seff", thisAssemblyPath,"RevitTemplate.EntryCommand")
+                     
+            let button = new PushButtonData("Seff", "Open Seff Editor Window", thisAssemblyPath, "Seff.Revit.OpenEditorCommand")
+            button.ToolTip <- "This will open the Seff Editor Window"
             
-            // defines the tooltip displayed when the button is hovered over in Revit's ribbon
-            button.ToolTip <- "Visual interface for debugging applications.";
-            
+            let panel = app.CreateRibbonPanel("Seff")
             //let uriImage = new Uri("pack://application:,,,/RevitTemplate;component/Resources/code-small.png");
             //let largeImage = new System.Windows.Media.Imaging.BitmapImage(uriImage);
             //button.LargeImage <- largeImage
             panel.AddItem(button) |> ignore 
-
-            //https://thebuildingcoder.typepad.com/blog/2018/11/revit-window-handle-and-parenting-an-add-in-form.html
-            let winHandle = Diagnostics.Process.GetCurrentProcess().MainWindowHandle
-
-            let seff = Seff.App.runEditorHosted(winHandle,"Revit")
-            //seff.Window.Show()
 
 
             Result.Succeeded
