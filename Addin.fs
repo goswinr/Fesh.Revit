e5517f29fa82a0b1caeb1f37de6a5b758f92a640
Goswin <gwinsky@yahoo.com>
2020-05-20 15:15:28 +0200

    still System.MissingMethodException

--- Addin.fs
+++ Addin.fs
@@ -7,47 +7,43 @@ open System
 open System.Windows
 open Seff
 
-// to acces the current UIControlledApplication
-type Current private () =
-    static member val UiApp: UIControlledApplication = null with get ,set
-    static member val App: UIApplication = null with get ,set
-    static member val Doc: Document = null with get ,set
-
-[<Transaction(TransactionMode.Manual)>]
-type StartEditorCommand() =
+// to acces the current UIControlledApplication : fails with System.MissingMethodException
+module Current =
+    let mutable UiApp: UIControlledApplication = null 
+    let mutable App: UIApplication = null
+    let mutable Doc: Document = null 
+    let mutable SeffWin: Window = null 
+    let run(f:Document -> unit) = f(Doc)
     
-    let mutable win :Window = null
-
-    interface IExternalCommand with
+[<Transaction(TransactionMode.Manual)>]
+type StartEditorCommand() = //new instance is created on every button click
+    
+       interface IExternalCommand with
         member this.Execute(commandData: ExternalCommandData, message: byref<string>, elements: ElementSet): Result = 
-            try
-                
-                if isNull win then 
-                    
+            try                
+                if isNull Current.SeffWin then                     
                     Current.App <- commandData.Application
                     Current.Doc <- commandData.Application.ActiveUIDocument.Document
 
+                    TaskDialog.Show("Seff", sprintf "Current.Doc:%A" Current.Doc) |> ignore 
+
                     //https://thebuildingcoder.typepad.com/blog/2018/11/revit-window-handle-and-parenting-an-add-in-form.html
                     let winHandle = Diagnostics.Process.GetCurrentProcess().MainWindowHandle
-                    
-                    TaskDialog.Show("Seff", sprintf "creating" ) |> ignore 
                     let seff = Seff.App.runEditorHosted(winHandle,"Revit")
-                    win <- seff.Window
+                    Current.SeffWin <- seff.Window
                     
-                    win.Closing.Add (fun e -> 
+                    Current.SeffWin.Closing.Add (fun e -> 
                         match seff.Fsi.AskAndCancel() with
-                        |Evaluating                      -> e.Cancel <- true // no closing
+                        |Evaluating -> e.Cancel <- true // no closing
                         |Ready | Initalizing | NotLoaded -> 
-                            win.Visibility <- Visibility.Hidden 
-                            TaskDialog.Show("Seff", sprintf "win.Visibility: %A" win.Visibility ) |> ignore 
-                            //TODO add option to menu to actually close, not just hide ??
-                            e.Cancel <- true) 
+                            Current.SeffWin.Visibility <- Visibility.Hidden                           
+                            e.Cancel <- true) // no closing
+
                     //app.Run(seff.Window)  |> ignore 
-                    win.Show()
+                    Current.SeffWin.Show()
                     Result.Succeeded
-                else
-                    TaskDialog.Show("Seff", sprintf "win.Visibility: %A" win.Visibility ) |> ignore 
-                    win.Visibility <- Visibility.Visible
+                else                    
+                    Current.SeffWin.Visibility <- Visibility.Visible
                     Result.Succeeded
 
             with ex ->
@@ -58,36 +54,39 @@ type StartEditorCommand() =
 type SeffAddin() = // don't rename !
     interface IExternalApplication with
         member this.OnStartup(app:UIControlledApplication) =
+            try
+                Sync.installSynchronizationContext()
+                Current.UiApp <- app
             
-            Sync.installSynchronizationContext()
-            Current.UiApp <- app
-            
-            // Method to add Tab and Panel 
-            let tabId = "Seff"
-            app.CreateRibbonTab(tabId)
+                // Method to add Tab and Panel 
+                let tabId = "Seff"
+                app.CreateRibbonTab(tabId)
 
-            let thisAssemblyPath = Reflection.Assembly.GetExecutingAssembly().Location
+                let thisAssemblyPath = Reflection.Assembly.GetExecutingAssembly().Location
 
      
-            let button = new PushButtonData("Seff", "Open Fsharp Editor", thisAssemblyPath, "Seff.Revit.StartEditorCommand")
-            button.ToolTip <- "This will open the Seff Editor Window"
-            try
+                let button = new PushButtonData("Seff", "Open Fsharp Editor", thisAssemblyPath, "Seff.Revit.StartEditorCommand")
+                button.ToolTip <- "This will open the Seff Editor Window"
+            
                 let uriImage = new Uri("pack://application:,,,/Seff.Revit;component/Media/LogoCursorTr32.png") // build from VS not via "dotnet build"  to include. <Resource Include="Media\LogoCursorTr32.png" />  
                 let largeImage = new System.Windows.Media.Imaging.BitmapImage(uriImage)
                 button.LargeImage <- largeImage
+            
+
+                let panel = app.CreateRibbonPanel(tabId,"Seff")
+            
+                panel.AddItem(button) |> ignore 
+                Result.Succeeded
             with ex ->
                 TaskDialog.Show("Seff", sprintf "%A" ex ) |> ignore 
-                
+                Result.Failed
 
-            let panel = app.CreateRibbonPanel(tabId,"Seff")
-            
-            panel.AddItem(button) |> ignore 
-
-
-            Result.Succeeded
         
         member this.OnShutdown(app:UIControlledApplication) =           
 
             Result.Succeeded
 
-            // System.Guid.NewGuid().ToString().ToUpper() // for FSI
+
+
+
+// System.Guid.NewGuid().ToString().ToUpper() // for FSI
