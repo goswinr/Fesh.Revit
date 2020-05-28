934159ec4063558c4c37e169f36b4f2c31b99299
Goswin <gwinsky@yahoo.com>
2020-05-28 13:10:30 +0200

    add error message abou how to load documents

--- Addin.fs
+++ Addin.fs
@@ -9,12 +9,20 @@ open Seff
 open System.Windows.Input
 open System.Reflection
 open System.Collections.Concurrent
+open System.Diagnostics
 
+
+module Debug =   
+    let log prefix (content:string) =
+        let time = DateTime.UtcNow.ToString("yyyy-MM-dd_HH-mm-ss-fff")        
+        let filename = sprintf "%sSeff.Revit.Log-%s.txt" prefix time
+        let file = IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop),filename)
+        async {try  IO.File.WriteAllText(file, content) with _ -> ()} |> Async.Start // file might be open and locked
 
  [<AbstractClass; Sealed>]
  type Current private ()=     
-     static member val internal SeffIsShown: bool     = false  with get,set
-     static member val          SeffWindow: Window    = null  with get, set 
+     static member val internal SeffWasEverShown: bool      = false with get,set
+     static member val          SeffWindow      : Window    = null  with get, set 
 
 
 type internal RunEvHandler(seff:Seff, queue: ConcurrentQueue< UIApplication->unit >) =    
@@ -33,26 +41,6 @@ type internal RunEvHandler(seff:Seff, queue: ConcurrentQueue< UIApplication->uni
         member this.GetName() = this.GetName()
 
 
-
-[<Regeneration(RegenerationOption.Manual)>]  
-[<Transaction(TransactionMode.Manual)>]
-type StartEditorCommand() = // don't rename ! string referenced in  PushButtonData //new instance is created on every button click    
-    interface IExternalCommand with
-        member this.Execute(commandData: ExternalCommandData, message: byref<string>, elements: ElementSet): Result = 
-            try                
-                if not Current.SeffIsShown then 
-                    Current.SeffWindow.Show()
-                    Current.SeffIsShown <- true
-                    Result.Succeeded
-                else                    
-                    Current.SeffWindow.Visibility <- Visibility.Visible
-                    Result.Succeeded
-
-            with ex ->
-                TaskDialog.Show("Execute Button Seff", sprintf "%A" ex ) |> ignore 
-                Result.Failed
-
-
 [<Regeneration(RegenerationOption.Manual)>]
 [<Transaction(TransactionMode.Manual)>]
 [<AllowNullLiteralAttribute>]
@@ -66,6 +54,10 @@ type SeffAddin() = // don't rename ! string referenced in in seff.addin file
 
     static member Instance = instance
     
+    //member this.Queue = queue
+
+    //member this.ExternalEvent  with get() = exEvent   and set(e) = exEvent <- e
+
     member this.RunApp (f:UIApplication->unit) =  
         queue.Enqueue(f)
         exEvent.Raise() |> ignore 
@@ -74,14 +66,13 @@ type SeffAddin() = // don't rename ! string referenced in in seff.addin file
         queue.Enqueue(fun app -> f(app.ActiveUIDocument.Document))
         exEvent.Raise() |> ignore 
     
-    // reverse a string
-    member this.Test(s:String) = s |> Seq.rev |> Seq.map string |> String.concat "" 
    
     interface IExternalApplication with
         member this.OnStartup(uiConApp:UIControlledApplication) =
             try
                 Sync.installSynchronizationContext()
-                           
+                instance <- this     
+                
                 // ------------------- create Ribbon and button -------------------------------------------
                 let thisAssemblyPath = Reflection.Assembly.GetExecutingAssembly().Location     
                 let button = new PushButtonData("Seff", "Open Fsharp Editor", thisAssemblyPath, "Seff.Revit.StartEditorCommand")
@@ -97,13 +88,21 @@ type SeffAddin() = // don't rename ! string referenced in in seff.addin file
                 let panel = uiConApp.CreateRibbonPanel(tabId,"Seff")            
                 panel.AddItem(button) |> ignore
                 
-                
 
                 //-------------- start Seff -------------------------------------------------------------                
                 //https://thebuildingcoder.typepad.com/blog/2018/11/revit-window-handle-and-parenting-an-add-in-form.html
                 let winHandle = Diagnostics.Process.GetCurrentProcess().MainWindowHandle
                 let seff= Seff.App.createEditorForHosting(winHandle,"Revit")
                 Current.SeffWindow <- seff.Window
+
+                //TODO make a C# plugin that loads Seff.addin once uiConApp.ControlledApplication.ApplicationInitialized to avoid missing method exceptions in FSI
+                //uiConApp.LoadAddIn
+                seff.Fsi.OnRuntimeError.Add (fun e -> 
+                    match e with 
+                    | :? MissingMethodException -> seff.Log.PrintFsiErrorMsg "*** To avoid this MissingMethodException exception try restarting Revit without a document.\r\n*** Then from within Revit open your desired project.\r\n*** If the error persits please report it!"
+                    | _ -> ()
+                    )
+
 
                 (* //TODO Alt enter does not work !
                 seff.Window.KeyDown.Add(fun e -> //to avoid pressing alt to focus on menu and the diabeling Alt+Enter for Evaluationg selection in FSI                       
@@ -130,29 +129,13 @@ type SeffAddin() = // don't rename ! string referenced in in seff.addin file
                     |Ready | Initalizing | NotLoaded -> 
                         seff.Window.Visibility <- Visibility.Hidden                           
                         e.Cancel <- true) // no closing
-                
-             
+                           
+                        
                 //-------------- hook up Seff ------------------------------------------------------------- 
                 let handler = RunEvHandler(seff, queue)
                 exEvent <- ExternalEvent.Create(handler)                
-                
-                //TaskDialog.Show("uiConApp:", sprintf "%A" uiConApp.ControlledApplication.VersionNumber ) |> ignore 
-                (*
-                let uiApp =
-                    let versionNumber = int uiConApp.ControlledApplication.VersionNumber
-                    let fieldName = if versionNumber >= 2017 then  "m_uiapplication" else "m_application"
-                    let fi = uiConApp.GetType().GetField(fieldName, BindingFlags.NonPublic ||| BindingFlags.Instance)
-                    fi.GetValue(uiConApp) :?> UIApplication 
-                uiApp.Idling.AddHandler(fun obj args -> 
-                    let app = obj :?> UIApplication
-                    let ok,f = Current.Queue.TryDequeue()
-                    if ok then 
-                        let doc = uiApp.ActiveUIDocument.Document
-                        f(doc)                    
-                        )  *)
-                
-                instance <- this
-
+                           
+                              
                 Result.Succeeded
 
             with ex ->
@@ -166,5 +149,32 @@ type SeffAddin() = // don't rename ! string referenced in in seff.addin file
 
 
 
+[<Regeneration(RegenerationOption.Manual)>]  
+[<Transaction(TransactionMode.Manual)>]
+type StartEditorCommand() = // don't rename ! string referenced in  PushButtonData //new instance is created on every button click    
+    interface IExternalCommand with
+        member this.Execute(commandData: ExternalCommandData, message: byref<string>, elements: ElementSet): Result = 
+            try                
+                if not Current.SeffWasEverShown then 
+                    
+                    //--------- now show() -------------------
+                    Current.SeffWindow.Show()
+                    Current.SeffWasEverShown <- true
+                    Result.Succeeded
+                else                    
+                    Current.SeffWindow.Visibility <- Visibility.Visible
+                    Result.Succeeded
+
+            with ex ->
+                TaskDialog.Show("Execute Button Seff", sprintf "%A" ex ) |> ignore 
+                Result.Failed
+
+
+            (* let uiApp =
+                 let versionNumber = int uiConApp.ControlledApplication.VersionNumber
+                 let fieldName = if versionNumber >= 2017 then  "m_uiapplication" else "m_application"
+                 let fi = uiConApp.GetType().GetField(fieldName, BindingFlags.NonPublic ||| BindingFlags.Instance)
+                 fi.GetValue(uiConApp) :?> UIApplication  *)
+                        
 
 // System.Guid.NewGuid().ToString().ToUpper() // for FSI
